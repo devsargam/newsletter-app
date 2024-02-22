c48eadd670116aa6a4e35ec7cb5104f40f87438e
devsargam <sargampoudel100@gmail.com>
2024-02-22 12:28:19 +0530

    feat: setup prisma client

---
+++ src/db/index.ts
@@ -0,0 +1,18 @@
+// Copied from https://github.com/devsargam/cms/blob/main/src/db/index.ts
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
