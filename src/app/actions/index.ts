2fa293d23737254fc929bf08cac5a3910a819e27
devsargam <sargampoudel100@gmail.com>
2024-02-25 18:01:49 +0530

    feat: setup add newsletter

---
+++ src/app/actions/index.ts
@@ -0,0 +1,68 @@
+'use server';
+import { z } from 'zod';
+import db from '@/db';
+import { revalidatePath } from 'next/cache';
+
+interface CreateNewsletterReturn {
+  message: string;
+  error: boolean;
+  description: string;
+}
+
+export async function createNewsletter(
+  prevState: {
+    message: string;
+    error: boolean;
+    description: string;
+  },
+  formData: FormData,
+): Promise<CreateNewsletterReturn> {
+  const schema = z.object({
+    title: z.string().min(10).max(50),
+    content: z.string().min(8).max(200),
+    email: z.string().email(),
+  });
+
+  const parse = schema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+    email: formData.get('email'),
+  });
+
+  if (!parse.success) {
+    return {
+      error: true,
+      message: 'Uh oh!',
+      description: parse.error.errors[0].message,
+    };
+  }
+
+  const data = parse.data;
+
+  try {
+    const newNewsletter = await db.newsletter.create({
+      data: {
+        title: data.title,
+        content: data.content,
+        createdBy: {
+          connect: {
+            email: data.email,
+          },
+        },
+      },
+    });
+
+    revalidatePath('/');
+    return {
+      error: false,
+      message: `Added newsletter`,
+      description: newNewsletter.title,
+    };
+  } catch (e) {
+    return {
+      error: true,
+      message: 'Uh Oh!',
+      description: 'Failed to create Newsletter',
+    };
+  }
+}
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,15 +1,19 @@
 import { MainNav } from '@/components/NavBar';
+import { CreateNewsletter } from '@/components/create-newsletter';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
 export default async function CreateNewsletterPage() {
   const session = await getServerSession();
 
-  if (!session) return redirect('/login');
+  if (!session?.user?.email) return redirect('/login');
 
   return (
     <>
       <MainNav />
+      <section className='flex justify-center py-10'>
+        <CreateNewsletter email={session.user.email} />
+      </section>
     </>
   );
 }
